47ad736e6232c67e82fdaa8239ebbe6336d7c7a1
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-14 23:39:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else katz_deli.length >= 1
-    line_currently = "The line is currently: "
+    line_currently = "The line is currently:"
     katz_deli.each_with_index do |name, index|
       line_currently << " #{index + 1}. #{name}"
     end
