003852e088deacd5852167683e77825cac662783
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-14 23:54:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(array, name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
   if katz_deli == 0
     puts "There is nobody waiting to be served!"
   end
